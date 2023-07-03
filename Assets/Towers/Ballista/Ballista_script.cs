6e41fcc8a923384df733886798f97e37ab8a9acb
AndreyZhvakin <azhvakin@solanteq.com>
2023-07-04 01:54:49 +0300

    Добавил башню Ballista, создал для неё анимации выстрела по всем направлениям, прицепил к ней тригер, Ballista записывает в список всех врагов в зоне её видимости

---
+++ Assets/Towers/Ballista/Ballista_script.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower_script : MonoBehaviour
+{
+    public Animator animator;
+
+    private List<GameObject> enemys_around = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isEmpty = !enemys_around.Any();
+        if (!isEmpty)
+        {
+            // rotate and fire 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //when a enemy enters
+        if (other.tag == "Enemy")
+        {
+            enemys_around.Add(other);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //when a enemy enters
+        if (other.tag == "Enemy")
+        {
+            enemys_around.Remove(other);
+        }
+    }
+}
