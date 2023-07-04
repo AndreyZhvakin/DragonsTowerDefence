89b19c98a071211bf704e8ea9e821d15fb59fdc3
AndreyZhvakin <azhvakin@solanteq.com>
2023-07-04 12:27:45 +0300

    Дракон теперь считаеть расстояние, которое он прошёл. Оно нужно для определения текущей цели башни

--- Assets/Enemy/base_dragon/base_behaviour.cs
+++ Assets/Enemy/base_dragon/base_behaviour.cs
@@ -11,31 +11,20 @@ public class base_behaviour : MonoBehaviour
 
     private int currentPoint = 0;
     private float lastWaypointSwitchTime;
+
     public float speed = 0.05f;
+
     private GameObject[] points;
 
-    private float pathLength;
+
+    public float RestOfPath = 0;
+    //private float pathLength;
 
     Vector3 startPosition;
     Vector3 endPosition;
 
-    // Start is called before the first frame update
-    void Start()
+    void ChooseAnimation()
     {
-        //get points of the path
-        PathLink = GameObject.Find("Path");
-        points = PathLink.GetComponent<Path>().Points;
-
-        //planning route at the first point
-        startPosition = points[currentPoint].transform.position;
-        endPosition = points[currentPoint + 1].transform.position;
-        pathLength = Vector3.Distance(startPosition, endPosition);
-    }
-
-    // Update is called once per frame
-    void Update()
-    { 
-        // animation
         Vector3 diration = endPosition - startPosition;
         if (diration.x > diration.y)
         {
@@ -55,19 +44,46 @@ public class base_behaviour : MonoBehaviour
             //left
             animator.SetInteger("diration", 2);
         }
-        
+    }
+
+    void planRoute()
+    {
+        //planning route at a point
+        startPosition = points[currentPoint].transform.position;
+        endPosition = points[currentPoint + 1].transform.position;
+        pathLength = Vector3.Distance(startPosition, endPosition);
 
         //unknown cause, z not equal 0 for points anytime 
         endPosition.z = 0;
+    }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get points of the path
+        PathLink = GameObject.Find("Path");
+        points = PathLink.GetComponent<Path>().Points;
 
+        //RestOfPath = PathLink.GetComponent<Path>().Lenght;
+        
         //move
+        planRoute();
+        ChooseAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move
+        Vector3 last_position = gameObject.transform.position;
+
         float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
         float totalTimeForPath = pathLength / speed;
         gameObject.transform.position = Vector2.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
 
+        RestOfPath -= Time.deltaTime*speed;
 
-        // on points
+        // on pointscurrent_position
         if (gameObject.transform.position.Equals(endPosition))
         { 
             //last point 
@@ -75,13 +91,15 @@ public class base_behaviour : MonoBehaviour
             {
                 Destroy(gameObject);
             }
+            else
+            {
+                //planning a route to the next point
+                currentPoint++;
+                lastWaypointSwitchTime = Time.time;
 
-            //planning a route to the next point
-            currentPoint++;
-            lastWaypointSwitchTime = Time.time;
-            startPosition = points[currentPoint].transform.position;
-            endPosition = points[currentPoint + 1].transform.position;
-            pathLength = Vector3.Distance(startPosition, endPosition);
+                planRoute();
+                ChooseAnimation();
+            }
         }
     }
 }
